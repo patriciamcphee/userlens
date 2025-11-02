5b9f9a9efadb2519cb85584b196b7eb2cafdf509
Patricia McPhee <hi@patriciamcphee.com>
2025-11-02 11:15:49 -0500

    update deploy script

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,3 +1,4 @@
-swa deploy ./dist \
-  --deployment-token 1b1be910152e9b8ede20496bf2930ffdf29c081e9c1eac7db03f087e77e9b54c03-86d2d011-dfea-49d3-9284-541e25c60834010291109cc2d410 \
+swa deploy dist \
+  --api-location api \
+  --deployment-token $AZURE_SWA_TOKEN \
   --env production
